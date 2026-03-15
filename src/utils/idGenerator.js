a7949c1657c6e4568dff13a28173158ec0ea8c55
revio1803-prog <revio1803@gmail.com>
2026-03-15 23:57:04 +0530

    Update idGenerator.js

--- src/utils/idGenerator.js
+++ src/utils/idGenerator.js
@@ -1,9 +1,15 @@
-function generateId(prefix, number) {
+function generateIdentifier(type, id){
 
-  const num = String(number).padStart(5, "0");
+let prefix = "RES";
 
-  return prefix + num;
+if(type === "author") prefix = "RES-AUTH";
+if(type === "dataset") prefix = "RES-DATA";
+if(type === "paper") prefix = "RES-PAPR";
+
+const num = String(id).padStart(5,"0");
+
+return `${prefix}-${num}`;
 
 }
 
-module.exports = generateId;
+module.exports = generateIdentifier;
