2e8570748fa0a493c70528961eabcd973905eb4d
revio1803-prog <revio1803@gmail.com>
2026-03-15 23:48:34 +0530

    Refactor authors route to return JSON response

--- src/routes/authors.js
+++ src/routes/authors.js
@@ -3,39 +3,14 @@ const router = express.Router();
 const pool = require("../config/db");
 
 router.get("/browse-authors", async (req, res) => {
+
   try {
 
     const result = await pool.query(
       "SELECT * FROM authors ORDER BY id DESC LIMIT 50"
     );
 
-    let rows = "";
-
-    result.rows.forEach(a => {
-
-      rows += `
-      <tr>
-        <td>${a.id}</td>
-        <td>${a.name}</td>
-        <td>${a.institution || ""}</td>
-        <td>${a.identifier || ""}</td>
-      </tr>
-      `;
-
-    });
-
-    res.send(`
-      <h2>Authors</h2>
-      <table border="1" cellpadding="5">
-      <tr>
-      <th>ID</th>
-      <th>Name</th>
-      <th>Institution</th>
-      <th>Identifier</th>
-      </tr>
-      ${rows}
-      </table>
-    `);
+    res.json(result.rows);
 
   } catch (err) {
 
@@ -43,6 +18,7 @@ router.get("/browse-authors", async (req, res) => {
     res.status(500).send("Server error");
 
   }
+
 });
 
 module.exports = router;
