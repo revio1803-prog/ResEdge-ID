52226dbb20c3272af48316e895d517fb59320d55
revio1803-prog <revio1803@gmail.com>
2026-03-15 23:46:05 +0530

    Update errorHandler.js

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,9 +1,11 @@
 function errorHandler(err, req, res, next) {
+
   console.error(err.stack);
 
   res.status(500).json({
     error: "Internal Server Error"
   });
+
 }
 
 module.exports = errorHandler;
