4ad3890bc36042e537618286122cc7f6b4774866
revio1803-prog <revio1803@gmail.com>
2026-03-15 23:58:55 +0530

    Update datasets.js

--- src/routes/datasets.js
+++ src/routes/datasets.js
@@ -57,3 +57,59 @@ res.status(500).send("Server Error")
 })
 
 module.exports = router
+
+router.get("/create-dataset",(req,res)=>{
+
+res.send(layout(
+"Create Dataset",
+`
+<h2>Create Dataset</h2>
+
+<form method="POST" action="/create-dataset">
+
+<label>Title</label><br>
+<input name="title" required><br><br>
+
+<label>Year</label><br>
+<input name="year"><br><br>
+
+<button class="btn">Create Dataset</button>
+
+</form>
+`
+))
+
+})
+
+const generateIdentifier = require("../utils/idGenerator");
+
+router.post("/create-dataset",async(req,res)=>{
+
+try{
+
+const {title,year} = req.body;
+
+const insert = await pool.query(
+"INSERT INTO datasets(title,year) VALUES($1,$2) RETURNING id",
+[title,year]
+);
+
+const id = insert.rows[0].id;
+
+const identifier = generateIdentifier("dataset",id);
+
+await pool.query(
+"UPDATE datasets SET identifier=$1 WHERE id=$2",
+[identifier,id]
+);
+
+res.redirect("/browse-datasets");
+
+}catch(err){
+
+console.error(err)
+res.status(500).send("Server Error")
+
+}
+
+})
