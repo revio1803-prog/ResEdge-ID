febd62afd7fd2014244dcf1773a1c3b7fae1525f
revio1803-prog <revio1803@gmail.com>
2026-03-15 23:33:38 +0530

    Initialize Express server with routing and middleware

---
+++ server.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+const express = require("express");
+const morgan = require("morgan");
+
+const authorsRoutes = require("./src/routes/authors");
+const datasetsRoutes = require("./src/routes/datasets");
+const identifiersRoutes = require("./src/routes/identifiers");
+
+const errorHandler = require("./src/middleware/errorHandler");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(morgan("dev"));
+
+app.use("/", authorsRoutes);
+app.use("/", datasetsRoutes);
+app.use("/", identifiersRoutes);
+
+app.get("/", (req, res) => {
+
+  res.send("Research Edge Identifier Server Running");
+
+});
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+
+  console.log("Server running on port " + PORT);
+
+});
