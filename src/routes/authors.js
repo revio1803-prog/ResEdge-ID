a0b2fa4a9687e9ea55b06db891a1049b8454f401
revio1803-prog <revio1803@gmail.com>
2026-03-15 23:53:31 +0530

    Update authors.js

--- src/routes/authors.js
+++ src/routes/authors.js
@@ -1,24 +1,59 @@
 const express = require("express");
 const router = express.Router();
+
 const pool = require("../config/db");
+const layout = require("../views/layout");
 
-router.get("/browse-authors", async (req, res) => {
+router.get("/browse-authors", async (req,res)=>{
 
-  try {
+try{
 
-    const result = await pool.query(
-      "SELECT * FROM authors ORDER BY id DESC LIMIT 50"
-    );
+const result = await pool.query(
+"SELECT * FROM authors ORDER BY id DESC LIMIT 50"
+);
 
-    res.json(result.rows);
+let rows = "";
 
-  } catch (err) {
+result.rows.forEach(a=>{
 
-    console.error(err);
-    res.status(500).send("Server error");
-
-  }
+rows += `
+<tr>
+<td>${a.id}</td>
+<td>${a.name || ""}</td>
+<td>${a.institution || ""}</td>
+<td>${a.identifier || ""}</td>
+</tr>
+`;
 
 });
 
-module.exports = router;
+res.send(layout(
+"Authors",
+`
+<h2>Registered Authors</h2>
+
+<table>
+
+<tr>
+<th>ID</th>
+<th>Name</th>
+<th>Institution</th>
+<th>Identifier</th>
+</tr>
+
+${rows}
+
+</table>
+`
+))
+
+}catch(err){
+
+console.error(err)
+res.status(500).send("Server Error")
+
+}
+
+})
+
+module.exports = router
