7be249998b0befb53001192213559931e91aa2bc
revio1803-prog <revio1803@gmail.com>
2026-03-17 00:17:17 +0530

    Update identifiers.js

--- src/routes/identifiers.js
+++ src/routes/identifiers.js
@@ -55,7 +55,7 @@ if(data.type === "DATA"){
 autoTarget = "/dataset/" + identifier;
 }
 
-if(data.type === "paper"){
+if(data.type === "PAPR"){
 autoTarget = "/paper/" + identifier;
 }
 
@@ -152,7 +152,7 @@ if(data.type === "DATA"){
 autoTarget = "/dataset/" + identifier;
 }
 
-if(data.type === "paper"){
+if(data.type === "PAPR"){
 autoTarget = "/paper/" + identifier;
 }
 
