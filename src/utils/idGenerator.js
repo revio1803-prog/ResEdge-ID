617a831783c7477292698b6576d3379eb24f2585
revio1803-prog <revio1803@gmail.com>
2026-03-17 00:22:49 +0530

    Update idGenerator.js

--- src/utils/idGenerator.js
+++ src/utils/idGenerator.js
@@ -8,12 +8,14 @@ const prefix = "10.1001";
 
 let code;
 
+/* TYPE MAPPING */
+
 if(type === "author") code = "AUTH";
 else if(type === "dataset") code = "DATA";
-else if(type === "paper") code = "paper";
+else if(type === "paper") code = "PAPR";
 else throw new Error("Invalid identifier type");
 
-/* find last number */
+/* FIND LAST NUMBER */
 
 const result = await pool.query(
 `SELECT number FROM identifiers
@@ -29,17 +31,19 @@ if(result.rows.length > 0){
 nextNumber = result.rows[0].number + 1;
 }
 
-/* format number */
+/* FORMAT NUMBER */
 
 const numberStr = String(nextNumber).padStart(5,"0");
 
-/* final identifier */
+/* FINAL IDENTIFIER */
 
 const identifier = `${prefix}/${code}${numberStr}`;
 
+/* RETURN FULL DATA */
+
 return {
-identifier: identifier,
-prefix: prefix,
+identifier,
+prefix,
 number: nextNumber,
 type: code
 };
