845ff3280910821eb8738aeab5bc4baf7914785a
revio1803-prog <revio1803@gmail.com>
2026-03-16 19:10:09 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+connectionString: process.env.DATABASE_URL,
+ssl: {
+rejectUnauthorized: false
+}
 });
 
 /* ==============================
@@ -14,6 +14,8 @@ AUTO CREATE TABLES
 async function initDB(){
 
 try{
+
+/* AUTHORS */
 
 await pool.query(`
 CREATE TABLE IF NOT EXISTS authors (
@@ -25,21 +27,28 @@ created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 `);
 
+/* DATASETS */
+
 await pool.query(`
 CREATE TABLE IF NOT EXISTS datasets (
 id SERIAL PRIMARY KEY,
 title TEXT NOT NULL,
+publisher TEXT,
 year TEXT,
 identifier TEXT UNIQUE,
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 `);
 
+/* IDENTIFIERS */
+
 await pool.query(`
 CREATE TABLE IF NOT EXISTS identifiers (
 id SERIAL PRIMARY KEY,
 identifier TEXT UNIQUE,
+prefix TEXT DEFAULT '10.1001',
 type TEXT,
+number INTEGER DEFAULT 0,
 target_url TEXT,
 metadata JSONB,
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -50,7 +59,7 @@ console.log("Database ready");
 
 }catch(err){
 
-console.error("Database init error:",err);
+console.error("Database init error:", err);
 
 }
 
