dea312cb9c3ab808867058bb79d6abd75f4e067a
revio1803-prog <revio1803@gmail.com>
2026-03-16 10:30:29 +0530

    Update idGenerator.js

--- src/utils/idGenerator.js
+++ src/utils/idGenerator.js
@@ -1,14 +1,37 @@
-function generateIdentifier(type, id){
+const pool = require("../config/db");
 
-let prefix = "RES";
+async function generateIdentifier(type){
 
-if(type === "author") prefix = "RES-AUTH";
-if(type === "dataset") prefix = "RES-DATA";
-if(type === "paper") prefix = "RES-PAPR";
+let prefix = "10.1001";
+let code = "";
 
-const num = String(id).padStart(5,"0");
+if(type === "author") code = "AUTH";
+if(type === "dataset") code = "DATA";
+if(type === "paper") code = "PAPR";
 
-return `${prefix}-${num}`;
+const result = await pool.query(
+`SELECT number FROM identifiers
+WHERE type=$1
+ORDER BY number DESC
+LIMIT 1`,
+[type]
+);
+
+let nextNumber = 1;
+
+if(result.rows.length > 0){
+nextNumber = result.rows[0].number + 1;
+}
+
+let numberStr = String(nextNumber).padStart(5,"0");
+
+let identifier = `${prefix}/${code}${numberStr}`;
+
+return {
+identifier,
+prefix,
+number: nextNumber
+};
 
 }
 
